21a001571eefa456a1302f001ac47034b44bfbe5
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-03-12 21:27:18 +0000

    feat(lib): add mkBoolOpt function

--- lib/default.nix
+++ lib/default.nix
@@ -2,6 +2,7 @@
 with lib; rec {
   mkOpt = type: default: description:
     mkOption {inherit type default description;};
+  mkBoolOpt = mkOpt types.bool;
   enabled = {enable = true;};
   disabled = {enable = false;};
 }
