ce311fd48fadc15e7b753bbb3be446cad738a4f9
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-03-12 22:45:25 +0000

    fix: update secrets with freya's new ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  freya = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPgu4nkMWlKrvhSImskDARyWwr1B0PLvKhfaczf+sBR";
+  freya = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKxjuFsM2NxkVEV+CbitMx1JlUp5JvR7bY1gWFx5EAk5";
   odin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHJFqOIXL2rj72/1lXfy4QRM1a+MmRzdQbK0NUpDL/z1";
   loki = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDkIYQ9LaTuLo7cuK/ZbdmSi8F+W6zRcxv9zwQ7kc8Lc";
   systems = [freya loki odin];
