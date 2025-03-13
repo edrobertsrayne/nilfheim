faeae6aabeea54950d065326115a68c30938f738
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-03-13 08:38:15 +0000

    feat: update alacritty config

--- modules/nixos/desktop/addons/alacritty/default.nix
+++ modules/nixos/desktop/addons/alacritty/default.nix
@@ -12,20 +12,26 @@ in {
     enable = mkEnableOption "Whether to enable alacritty terminal emulator";
   };
   config = mkIf cfg.enable {
-    ${namespace}.home.extraOptions = {
-      programs.alacritty = {
-        enable = true;
-        settings = {
-          font.size = 12;
-          font.normal = {
-            family = "JetBrains Mono Nerd Font";
-            style = "Regular";
-          };
-          window = {
-            padding = {
-              x = 4;
-              y = 0;
+    ${namespace}.home = {
+      extraOptions = {
+        programs.alacritty = {
+          enable = true;
+          settings = {
+            font = {
+              size = 12;
+              normal = {
+                family = "JetBrains Mono Nerd Font";
+                style = "Regular";
+              };
             };
+            window = {
+              padding = {
+                x = 4;
+                y = 0;
+              };
+              dynamic_padding = true;
+            };
+            selection.save_to_clipboard = true;
           };
         };
       };
