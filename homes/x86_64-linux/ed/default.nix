164d19bf8914b98672a68f48a37c57cfccb37c74
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-03-13 08:38:26 +0000

    feat: add core utils to home-manager

--- homes/x86_64-linux/ed/default.nix
+++ homes/x86_64-linux/ed/default.nix
@@ -11,4 +11,20 @@ with lib.${namespace}; {
       git.enable = true;
     };
   };
+  programs = {
+    bat = enabled;
+    btop = enabled;
+    eza = enabled;
+    fd = enabled;
+    fzf = enabled;
+    jq = enabled;
+    lazygit = enabled;
+    lf = enabled;
+    password-store = enabled;
+    ripgrep = enabled;
+    thefuck = enabled;
+    tmux = enabled;
+    yazi = enabled;
+    zoxide = enabled;
+  };
 }
