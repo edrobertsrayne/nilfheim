dba362ee5c847f29de2d9107341f52f4d36c78c1
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-03-16 22:55:06 +0000

    feat: enable firefox persistence

--- nixos/desktop/firefox.nix
+++ nixos/desktop/firefox.nix
@@ -1,17 +1,35 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
-with lib; let
+with lib;
+with lib.custom; let
   cfg = config.modules.desktop.firefox;
-  inherit (config.modules) user;
+  inherit (config.modules.system) persist;
 in {
-  options.modules.desktop.firefox.enable = mkEnableOption "Whether to enable Firefox browser.";
-
-  config = mkIf cfg.enable {
-    home-manager.users.${user.name}.config = {
-      programs.firefox = {enable = true;};
-    };
+  options.modules.desktop.firefox = {
+    enable = mkEnableOption "Whether to enable Firefox browser.";
+    defaultBrowser = mkBoolOpt true "Whether to set Firefox as the default browser.";
   };
+
+  config = mkIf cfg.enable (mkMerge
+    [
+      {
+        programs.firefox = {
+          enable = true;
+          package = pkgs.firefox;
+        };
+        xdg.mime.defaultApplications = mkIf cfg.defaultBrowser {
+          "text/html" = "firefox.desktop";
+          "x-scheme-handler/http" = "firefox.desktop";
+          "x-scheme-handler/https" = "firefox.desktop";
+        };
+      }
+
+      (mkIf persist.enable {
+        modules.system.persist.extraUserDirectories = ["./mozilla/firefox"];
+      })
+    ]);
 }
