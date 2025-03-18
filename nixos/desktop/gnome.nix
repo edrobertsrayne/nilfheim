ec5d590f9262df21c1f91713e64978768f1d1dff
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-03-18 06:51:59 +0000

    feat: gnome configuration tweaks

--- nixos/desktop/gnome.nix
+++ nixos/desktop/gnome.nix
@@ -7,8 +7,17 @@
 with lib;
 with lib.custom; let
   cfg = config.modules.desktop.gnome;
+  inherit (config.modules) user desktop;
   defaultExtensions = with pkgs.gnomeExtensions; [
     gtile
+    dash-to-dock
+    caffeine
+    vitals
+    blur-my-shell
+    impatience
+    clipboard-indicator
+    removable-drive-menu
+    media-controls
     just-perfection
     logo-menu
     space-bar
@@ -18,7 +27,6 @@ with lib.custom; let
 in {
   options.modules.desktop.gnome = with types; {
     enable = mkEnableOption "Whether to enable GNOME desktop environment.";
-    color-scheme = mkOpt (enum ["light" "dark"]) "dark" "The colourscheme to use.";
     wayland = mkBoolOpt true "Whether to use Wayland.";
     autoSuspend = mkBoolOpt true "Whether to suspend the machine after inactivity.";
     extensions = mkOpt (listOf package) [] "Extra Gnome extensions to install.";
@@ -73,5 +81,64 @@ in {
     };
 
     programs.dconf.enable = true;
+
+    home-manager.users.${user.name}.config = {
+      dconf.settings = {
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = builtins.map (extension: extension.extensionUuid) (cfg.extensions ++ defaultExtensions);
+          favorite-apps =
+            [
+              "org.gnome.Nautilus.desktop"
+            ]
+            ++ optional desktop.firefox.enable "firefox.desktop"
+            ++ optional desktop.foot.enable "foot.desktop";
+        };
+        "org/gnome/desktop/peripherals/mouse" = {
+          speed = 0.0;
+          accel-profile = "flat";
+          natural-scroll = true;
+        };
+        "org/gnome/desktop/peripherals/touchpad" = {
+          disable-while-typing = false;
+        };
+        "org/gnome/desktop/wm/keybindings" = {
+          switch-to-workspace-1 = ["<Super>1"];
+          switch-to-workspace-2 = ["<Super>2"];
+          switch-to-workspace-3 = ["<Super>3"];
+          switch-to-workspace-4 = ["<Super>4"];
+          switch-to-workspace-5 = ["<Super>5"];
+          switch-to-workspace-6 = ["<Super>6"];
+          switch-to-workspace-7 = ["<Super>7"];
+          switch-to-workspace-8 = ["<Super>8"];
+          switch-to-workspace-9 = ["<Super>9"];
+          switch-to-workspace-10 = ["<Super>0"];
+
+          move-to-workspace-1 = ["<Shift><Super>1"];
+          move-to-workspace-2 = ["<Shift><Super>2"];
+          move-to-workspace-3 = ["<Shift><Super>3"];
+          move-to-workspace-4 = ["<Shift><Super>4"];
+          move-to-workspace-5 = ["<Shift><Super>5"];
+          move-to-workspace-6 = ["<Shift><Super>6"];
+          move-to-workspace-7 = ["<Shift><Super>7"];
+          move-to-workspace-8 = ["<Shift><Super>8"];
+          move-to-workspace-9 = ["<Shift><Super>9"];
+          move-to-workspace-10 = ["<Shift><Super>0"];
+        };
+        "org/gnome/shell/keybindings" = {
+          # Remove the default hotkeys for opening favorited applications.
+          switch-to-application-1 = [];
+          switch-to-application-2 = [];
+          switch-to-application-3 = [];
+          switch-to-application-4 = [];
+          switch-to-application-5 = [];
+          switch-to-application-6 = [];
+          switch-to-application-7 = [];
+          switch-to-application-8 = [];
+          switch-to-application-9 = [];
+          switch-to-application-10 = [];
+        };
+      };
+    };
   };
 }
