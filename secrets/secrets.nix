2abac85f50696e312a26bef18b881bf35a921f13
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-03-23 22:41:01 +0000

    chore: update loki agenix ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   odin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHJFqOIXL2rj72/1lXfy4QRM1a+MmRzdQbK0NUpDL/z1";
-  loki = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM3mLwuioaT6TJf0+xi6hxhj6ATR7CEN6+ehX/q/1Fcv";
+  loki = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICmanxD3yVEHjRsz2ZNOL7l5skXX3ytrm/uYO9PHNF+Y";
   freya = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL3SNrlpGasU1b0I3e6ynmqeuWKeT8gZjVAjKZxHMXaG";
   systems = [freya loki odin];
   users = [
