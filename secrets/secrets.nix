213a7926dfa4929422764600b556e892d10c85b8
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-03-25 07:26:56 +0000

    chore: rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,10 @@
 let
   odin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHJFqOIXL2rj72/1lXfy4QRM1a+MmRzdQbK0NUpDL/z1";
   loki = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICmanxD3yVEHjRsz2ZNOL7l5skXX3ytrm/uYO9PHNF+Y";
-  freya = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL3SNrlpGasU1b0I3e6ynmqeuWKeT8gZjVAjKZxHMXaG";
+  freya = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIO4RjgGmeN0QCTS8V5raJwcoxajuh2K60jtAhw2El1";
   systems = [freya loki odin];
   users = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHjO/+Q0fcuPJlilQNFfTbxG78ov3owvJW66poCTZVy4 
-"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJdf/364Rgul97UR6vn4caDuuxBk9fUrRjfpMsa4sfam ed@freya"
   ];
 in {
   "tailscale.age".publicKeys = systems ++ users;
