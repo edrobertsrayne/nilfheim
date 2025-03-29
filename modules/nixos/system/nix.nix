a171e0c011ed89d32e559d9dd53d1fd3997c6205
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-03-29 16:26:25 +0000

    chore: disable dirty git tree warning

--- modules/nixos/system/nix.nix
+++ modules/nixos/system/nix.nix
@@ -16,6 +16,7 @@ in {
       settings = {
         experimental-features = ["nix-command" "flakes"];
         auto-optimise-store = true;
+        warn-dirty = false;
       };
       gc = {
         automatic = true;
