f676a5307a648438426d9bcbf5eb9eff14e7a220
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-05-10 19:17:08 +0100

    put services into the tank group

--- modules/nixos/services/calibre.nix
+++ modules/nixos/services/calibre.nix
@@ -12,24 +12,21 @@ in {
     url = mkOpt types.str "calibre.${config.homelab.domain}" "Proxy address for calibre web server.";
   };
   config = mkIf cfg.enable {
-    users.users.${config.services.calibre-server.user}.extraGroups = ["tank"];
-    users.users.${config.services.calibre-web.user}.extraGroups = ["tank"];
-
     services = {
       calibre-server = {
         enable = true;
+        group = "tank";
         libraries = [
           "/mnt/media/calibre"
         ];
         port = 8090;
-        openFirewall = true;
       };
 
       calibre-web = {
         enable = true;
+        group = "tank";
         listen.ip = "0.0.0.0";
         dataDir = "/srv/calibre";
-        openFirewall = true;
         options = {
           calibreLibrary = "/mnt/media/calibre";
           enableBookUploading = true;
