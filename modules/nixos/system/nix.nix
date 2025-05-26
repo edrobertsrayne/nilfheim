813ef4d56f8d14c42d0b5f8bdf5e77d2f4ab6d8c
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-05-26 13:35:45 +0100

    feat: add proxmox and nix-community build caches

--- modules/nixos/system/nix.nix
+++ modules/nixos/system/nix.nix
@@ -17,6 +17,15 @@ in {
         experimental-features = ["nix-command" "flakes"];
         auto-optimise-store = true;
         warn-dirty = false;
+        substituters = [
+          "https://cache.nixos.org"
+          "https://nix-community.cachix.org"
+          "https://cache.saumon.network/proxmox-nixos"
+        ];
+        trusted-public-keys = [
+          "proxmox-nixos:nveXDuVVhFDRFx8Dn19f1WDEaNRJjPrF2CPD2D+m1ys="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
       };
       gc = {
         automatic = true;
