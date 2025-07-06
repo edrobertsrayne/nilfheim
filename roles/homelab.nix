42dc36ac5339b709022e8ba4c51c6c7f4ba51105
Ed Roberts Rayne <96839304+edrobertsrayne@users.noreply.github.com>
2025-07-06 08:29:30 +0100

    fix: add true to tailscale LAN access

--- roles/homelab.nix
+++ roles/homelab.nix
@@ -39,11 +39,11 @@ with lib.custom; {
         useRoutingFeatures = "server";
         extraUpFlags = [
           "--exit-node 10.71.91.83"
-          "--exit-node-allow-lan-access"
+          "--exit-node-allow-lan-access=true"
           ''--advertise-routes "192.168.68.0/24"''
         ];
         extraSetFlags = [
-          "--exit-node-allow-lan-access"
+          "--exit-node-allow-lan-access=true"
         ];
       };
       uptime-kuma.enable = true;
