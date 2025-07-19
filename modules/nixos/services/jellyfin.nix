8c01b459e27ac481bde80550563c53325b222acc
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-07-19 18:42:19 +0100

    fix(jellyfin): switch from intel-media-sdk to intel-media-driver

--- modules/nixos/services/jellyfin.nix
+++ modules/nixos/services/jellyfin.nix
@@ -81,7 +81,7 @@ in {
         libvdpau-va-gl
         intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
         vpl-gpu-rt # QSV on 11th gen or newer
-        intel-media-sdk # QSV up to 11th gen
+        intel-media-driver # QSV up to 11th gen
       ];
     };
   };
