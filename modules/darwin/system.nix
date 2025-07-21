3a7984eacdfc3f90abcda65200d0941c0830a645
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-07-21 14:06:16 +0100

    feat: add x86_64-linux builder to darwin systems

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -11,6 +11,30 @@
     settings = {
       experimental-features = ["nix-command" "flakes"];
       trusted-users = ["root" "@admin"];
+      extra-platforms = ["x86_64-linux" "aarch64-linux"];
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+
+    linux-builder = {
+      enable = true;
+      ephemeral = true;
+      maxJobs = 4;
+      config = {
+        virtualisation = {
+          darwin-builder = {
+            diskSize = 40 * 1024;
+            memorySize = 8 * 1024;
+          };
+          cores = 6;
+        };
+      };
     };
   };
 
