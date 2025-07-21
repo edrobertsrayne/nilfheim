085823655a2e28088489ac31aa48526354e7a72a
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-07-21 19:19:44 +0100

    feat(darwin): enable catppuccin theme and configure alacritty

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -7,8 +7,33 @@ _: {
       shell.enableShellIntegration = true;
     };
 
+    catppuccin = {
+      flavor = "mocha";
+      enable = true;
+    };
+
     programs = {
-      alacritty.enable = true;
+      alacritty = {
+        enable = true;
+        settings = {
+          font = {
+            size = 12.0;
+            normal = {
+              family = "JetBrainsMono Nerd Font";
+              style = "Regular";
+            };
+          };
+          window = {
+            opacity = 0.9;
+            padding = {
+              x = 4;
+              y = 0;
+            };
+            dynamic_padding = true;
+          };
+          selection.save_to_clipboard = true;
+        };
+      };
       ghostty.enable = false; # package marked as broken
       git = {
         enable = true;
