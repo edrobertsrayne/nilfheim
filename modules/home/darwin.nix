eccec7dbec088339653bab26b1da15dbace53a71
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-07-21 22:15:31 +0100

    feat(darwin): add wezterm config

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -40,7 +40,41 @@ _: {
         userName = "Ed Roberts Rayne";
         userEmail = "ed.rayne@gmail.com";
       };
-      wezterm.enable = true;
+      wezterm = {
+        enable = true;
+        extraConfig = ''
+          local mylib = require 'mylib'
+          return {
+                  enable_wayland = false,
+                  enable_tab_bar = false,
+                  font_size = 12.0,
+                  font = wezterm.font("JetBrainsMono Nerd Font"),
+                  macos_window_background_blur = 30,
+                  window_background_opacity = 0.95,
+                  window_decorations = "TITLE | RESIZE",
+                  warn_about_missing_glyphs = false,
+                  keys = {
+                          {
+                                  key = "f",
+                                  mods = "CTRL",
+                                  action = wezterm.action.ToggleFullScreen,
+                          },
+                          {
+                                  key = "'",
+                                  mods = "CTRL",
+                                  action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+                          },
+                  },
+                  mouse_bindings = {
+                          {
+                                  event = { Up = { streak = 1, button = "Left" } },
+                                  mods = "CTRL",
+                                  action = wezterm.action.OpenLinkAtMouseCursor,
+                          },
+                  },
+          }
+        '';
+      };
     };
   };
 }
