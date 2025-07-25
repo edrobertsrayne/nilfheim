75343a21cfcf7f294cb32a987a4387b67acad1c6
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-07-25 23:38:00 +0100

    feat(vps): add portainer

--- roles/vps.nix
+++ roles/vps.nix
@@ -1,22 +1,27 @@
-{
+{lib, ...}: {
   imports = [./server.nix];
 
-  virtualisation.oci-containers.containers = {
-    portainer = {
-      image = "portainer/portainer-ce:latest";
-      volumes = [
-        "portainer_data:/data"
-        "/var/run/docker.sock:/var/run/docker.sock"
-      ];
-      ports = [
-        "9443:9443"
-        "8000:8000"
-      ];
-      autoStart = true;
-      extraOptions = [
-        "--pull=always"
-        "--restart=unless-stopped"
-      ];
+  virtualisation = {
+    docker.rootless.enable = lib.mkForce false;
+    oci-containers = {
+      backend = "docker";
+      containers = {
+        portainer = {
+          image = "portainer/portainer-ce:latest";
+          volumes = [
+            "portainer_data:/data"
+            "/var/run/docker.sock:/var/run/docker.sock"
+          ];
+          ports = [
+            "9443:9443"
+            "8000:8000"
+          ];
+          autoStart = true;
+          extraOptions = [
+            "--pull=always"
+          ];
+        };
+      };
     };
   };
 }
