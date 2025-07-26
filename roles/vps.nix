27416cf7cd75c787b8f9b1826182949fb4129b73
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-07-27 00:30:16 +0100

    feat(vps): open ports and add dokploy persistence

--- roles/vps.nix
+++ roles/vps.nix
@@ -1,6 +1,9 @@
 {lib, ...}: {
   imports = [./server.nix];
 
+  networking.firewall.allowedTCPPorts = [80 443 3000 8080];
+  networking.firewall.allowedUDPPorts = [443];
+  system.persist.extraRootDirectories = ["/etc/dokploy" "/var/lib/docker"];
   virtualisation = {
     docker.rootless.enable = lib.mkForce false;
     oci-containers = {
