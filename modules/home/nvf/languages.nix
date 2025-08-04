cae24f614127b858e8e5f2c0c587433048a5a930
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-08-04 10:38:08 +0100

    feat: add golang to nvim

--- modules/home/nvf/languages.nix
+++ modules/home/nvf/languages.nix
@@ -13,7 +13,6 @@
           quickfix = "<leader>xQ";
         };
       };
-      # lspSignature.enable = true;
     };
     languages = {
       enableFormat = true;
@@ -21,46 +20,12 @@
       enableExtraDiagnostics = true;
 
       bash.enable = true;
+      go.enable = true;
       lua.enable = true;
-      nix = {
-        enable = true;
-      };
+      nix.enable = true;
       markdown.enable = true;
       python.enable = true;
       yaml.enable = true;
     };
-    # diagnostics = {
-    #   enable = true;
-    #   nvim-lint = {
-    #     enable = true;
-    #     linters_by_ft = {
-    #       javascript = ["eslint_d"];
-    #       typescript = ["eslint_d"];
-    #       python = ["ruff"];
-    #       lua = ["luacheck"];
-    #     };
-    #   };
-    # };
-    # formatter = {
-    #   conform-nvim = {
-    #     enable = true;
-    #     setupOpts = {
-    #       formatters_by_ft = {
-    #         lua = ["stylua"];
-    #         nix = ["alejandra"];
-    #         javascript = ["prettier"];
-    #         typescript = ["prettier"];
-    #         python = ["ruff_format"];
-    #         markdown = ["prettier"];
-    #         yaml = ["prettier"];
-    #         json = ["prettier"];
-    #       };
-    #       format_on_save = {
-    #         lsp_format = "fallback";
-    #         timeout_ms = 500;
-    #       };
-    #     };
-    #   };
-    # };
   };
 }
