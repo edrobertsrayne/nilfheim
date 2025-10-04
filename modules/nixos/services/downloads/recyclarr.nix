47e4d1c76ef512efc2c284a8045f2a062ced33d2
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-10-04 11:13:39 +0100

    feat(recyclarr): use example 1080p configs for sonarr and radarr

--- modules/nixos/services/downloads/recyclarr.nix
+++ modules/nixos/services/downloads/recyclarr.nix
@@ -2,19 +2,27 @@
   cfg = config.services;
 in {
   services.recyclarr.configuration = {
-    radarr = [
-      {
+    radarr = {
+      remux-web-1080p = {
         api_key = cfg.radarr.apikey;
         base_url = "http://localhost:${toString cfg.radarr.settings.server.port}";
-        instance_name = "main";
-      }
-    ];
-    sonarr = [
-      {
+        include = [
+          {template = "radarr-quality-definition-movie";}
+          {template = "radarr-quality-profile-remux-web-1080p";}
+          {template = "radarr-custom-formats-remux-web-1080p";}
+        ];
+      };
+    };
+    sonarr = {
+      web-1080p = {
         api_key = cfg.sonarr.apikey;
         base_url = "http://localhost:${toString cfg.sonarr.settings.server.port}";
-        instance_name = "main";
-      }
-    ];
+        include = [
+          {template = "sonarr-quality-definition-series";}
+          {template = "sonarr-v4-quality-profile-web-1080p";}
+          {template = "sonarr-v4-custom-formats-web-1080p";}
+        ];
+      };
+    };
   };
 }
