4c55bc542e3fbc21c019faa1763fd161d4134779
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-10-09 23:09:01 +0100

    fix(recyclarr): update sonarr quality profile with custom WEB-1080p settings

--- modules/nixos/services/downloads/recyclarr.nix
+++ modules/nixos/services/downloads/recyclarr.nix
@@ -3,13 +3,13 @@
 in {
   services.recyclarr.configuration = {
     radarr = {
-      remux-web-1080p = {
+      hd-bluray-web = {
         api_key = cfg.radarr.apikey;
         base_url = "http://localhost:${toString cfg.radarr.settings.server.port}";
         include = [
           {template = "radarr-quality-definition-movie";}
-          {template = "radarr-quality-profile-remux-web-1080p";}
-          {template = "radarr-custom-formats-remux-web-1080p";}
+          {template = "radarr-quality-profile-hd-bluray-web";}
+          {template = "radarr-custom-formats-hd-bluray-web";}
         ];
       };
     };
@@ -19,8 +19,30 @@ in {
         base_url = "http://localhost:${toString cfg.sonarr.settings.server.port}";
         include = [
           {template = "sonarr-quality-definition-series";}
-          {template = "sonarr-v4-quality-profile-web-1080p";}
           {template = "sonarr-v4-custom-formats-web-1080p";}
+        ];
+        quality_profiles = [
+          {
+            name = "WEB-1080p";
+            upgrade = {
+              allowed = true;
+              until_quality = "WEB-DL-1080p";
+            };
+            qualities = [
+              {
+                name = "WEB-DL-1080p";
+                qualities = ["WEBDL-1080p" "WEBRip-1080p"];
+              }
+              {name = "Bluray-1080p";}
+              {name = "HDTV-1080p";}
+              {
+                name = "WEB-DL-720p";
+                qualities = ["WEBDL-720p" "WEBRip-720p"];
+              }
+              {name = "Bluray-720p";}
+              {name = "HDTV-720p";}
+            ];
+          }
         ];
       };
     };
