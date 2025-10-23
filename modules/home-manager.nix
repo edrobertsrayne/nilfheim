7d9ab9dcbee25cec85a0b19e0e86c33ccc4111a8
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-10-23 17:57:43 +0100

    zsh shell for managed user but bash otherwise

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,7 +1,14 @@
 {inputs, ...}: {
-  flake.modules.nixos.home-manager = {config, ...}: let
+  flake.modules.nixos.home-manager = {
+    config,
+    pkgs,
+    ...
+  }: let
     inherit (config.networking) hostName;
   in {
+    users.users.ed.shell = pkgs.zsh;
+    programs.zsh.enable = true;
+
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
