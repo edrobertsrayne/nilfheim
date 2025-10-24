756c74a15560775bb9d4b299b9a0334aa9a18a86
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-10-24 20:57:25 +0000

    add a basic freya config

---
+++ modules/hosts/freya/default.nix
@@ -0,0 +1,26 @@
+{inputs, ...}: {
+  flake.nixosConfigurations.freya = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+
+    modules = with inputs.self.modules.nixos; [
+      ./_disko.nix
+      ./_hardware-configuration.nix
+      inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t480s
+
+      home-manager
+      nixos
+      desktop
+
+      {
+        networking.hostName = "freya";
+        networking.hostId = builtins.substring 0 8 (
+          builtins.hashString "sha256" "freya"
+        );
+      }
+    ];
+  };
+
+  flake.modules.homeManager.freya = {
+    imports = with inputs.self.modules.homeManager; [core desktop];
+  };
+}
