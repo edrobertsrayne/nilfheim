284c6d3de2393bf949753fcc68f458b90d298adc
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-10-29 23:43:26 +0000

    chore: remove comments

--- modules/nvf/navigation.nix
+++ modules/nvf/navigation.nix
@@ -3,14 +3,9 @@ _: {
     programs.nvf = {
       settings = {
         vim = {
-          # ===== NAVIGATION & SEARCH =====
-          # Telescope: Fuzzy finder for files, text, symbols, and more
           telescope = {
             enable = true;
           };
-
-          # Which-key: Shows available keybindings in a popup as you type
-          # Essential for discovering commands and learning keymaps
           binds = {
             whichKey = {
               enable = true;
--- modules/nvf/utilities.nix
+++ modules/nvf/utilities.nix
@@ -3,23 +3,16 @@ _: {
     programs.nvf = {
       settings = {
         vim = {
-          # ===== UTILITY PLUGINS =====
-          # Automatically close brackets, quotes, and other pairs
           autopairs = {
             nvim-autopairs.enable = true;
           };
-
-          # Easy commenting/uncommenting of code
           comments = {
             comment-nvim.enable = true;
           };
-
-          # Manipulate surrounding characters (quotes, brackets, tags)
-          # Example: cs"' changes "hello" to 'hello'
           utility = {
             surround.enable = true;
             motion = {
-              hop.enable = true; # Quick jump navigation - press a key combo and jump to any visible word
+              hop.enable = true;
             };
           };
         };
