ec543775927a6351ccc0aa1f01ade06ce9a423df
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-10-31 19:29:08 +0000

    feat(utilities): add nh nix helper

---
+++ modules/utilities/nh.nix
@@ -0,0 +1,13 @@
+_: {
+  flake.modules.homeManager.utilities = {
+    programs.nh = {
+      enable = true;
+      flake = "github:edrobertsrayne/nilfheim";
+      clean = {
+        enable = true;
+        dates = "daily";
+        extraArgs = "--keep 3 --keep-since 5d";
+      };
+    };
+  };
+}
