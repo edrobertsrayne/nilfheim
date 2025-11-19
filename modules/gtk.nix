f35fe6ba9f86ed8b3dc6ead6ca2df348e325951c
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-11-19 13:21:18 +0000

    fix(stylix): remove gtk iconTheme duplication

--- modules/gtk.nix
+++ modules/gtk.nix
@@ -2,10 +2,6 @@ _: {
   flake.modules.homeManager.gtk = {pkgs, ...}: {
     gtk = {
       enable = true;
-      iconTheme = {
-        name = "Papirus-Dark";
-        package = pkgs.papirus-icon-theme;
-      };
     };
   };
 }
