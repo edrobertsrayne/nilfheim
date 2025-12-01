00289c86758bd611e7bebc7b6665cfadb02f925b
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-12-01 23:07:31 +0000

    feat(waybar): apply matugen themeing

--- modules/matugen.nix
+++ modules/matugen.nix
@@ -8,20 +8,35 @@
       inputs.matugen.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
 
-    xdg.configFile."matugen/config.toml".text = ''
-      [config]
+    xdg = {
+      configFile."matugen/config.toml".text = ''
+        [config]
 
-      [templates.hyprland]
-      input_path = '${config.xdg.dataHome}/matugen/hyprland.conf'
-      output_path = '~/.config/hypr/colors.conf'
-      post_hook = 'hyprctl reload'
-    '';
+        [templates.hyprland]
+        input_path = '${config.xdg.dataHome}/matugen/hyprland.conf'
+        output_path = '${config.xdg.configHome}/hypr/colors.conf'
+        post_hook = 'hyprctl reload'
 
-    xdg.dataFile."matugen/hyprland.conf".text = ''
-      <* for name, value in colors *>
-      $image = {{image}}
-      ''${{name}} = rgba({{value.default.hex_stripped}}ff)
-      <* endfor *>
-    '';
+        [templates.waybar]
+        input_path = '${config.xdg.dataHome}/matugen/colors.css'
+        output_path = '${config.xdg.configHome}/waybar/colors.css'
+        post_hook = 'pkill -SIGUSR2 waybar'
+      '';
+
+      dataFile = {
+        "matugen/hyprland.conf".text = ''
+          <* for name, value in colors *>
+          $image = {{image}}
+          ''${{name}} = rgba({{value.default.hex_stripped}}ff)
+          <* endfor *>
+        '';
+
+        "matugen/colors.css".text = ''
+          <* for name, value in colors *>
+            @define-color {{name}} {{value.default.hex}};
+          <* endfor *>
+        '';
+      };
+    };
   };
 }
--- modules/waybar/waybar.nix
+++ modules/waybar/waybar.nix
@@ -1,152 +1,147 @@
-_: {
-  flake.modules.homeManager.waybar = {
-    pkgs,
-    lib,
-    config,
-    ...
-  }: {
-    stylix.targets.waybar.enable = false;
-    programs.waybar = {
-      enable = true;
-      settings = {
-        mainBar = {
-          reload_style_on_change = true;
-          layer = "top";
-          position = "top";
-          spacing = 0;
-          margin-top = 12;
-          margin-left = 8;
-          margin-right = 8;
+{inputs, ...}: {
+  flake.modules.homeManager.waybar = let
+    inherit (inputs.self.niflheim) fonts;
+  in
+    {
+      pkgs,
+      lib,
+      ...
+    }: {
+      stylix.targets.waybar.enable = false;
+      programs.waybar = {
+        enable = true;
+        settings = {
+          mainBar = {
+            reload_style_on_change = true;
+            layer = "top";
+            position = "top";
+            spacing = 0;
+            margin-top = 12;
+            margin-left = 8;
+            margin-right = 8;
 
-          modules-left = [
-            "hyprland/workspaces"
-            "mpris"
-            "hyprland/submap"
-          ];
-          modules-center = ["clock"];
-          modules-right = [
-            "pulseaudio"
-            "network"
-            "battery"
-          ];
+            modules-left = [
+              "hyprland/workspaces"
+              "mpris"
+              "hyprland/submap"
+            ];
+            modules-center = ["clock"];
+            modules-right = [
+              "pulseaudio"
+              "network"
+              "battery"
+            ];
 
-          "hyprland/workspaces" = {
-            on-click = "activate";
-            sort-by-number = true;
-            format = "{icon}";
-            format-icons = {
-              "spotify" = "󰓇";
+            "hyprland/workspaces" = {
+              on-click = "activate";
+              sort-by-number = true;
+              format = "{icon}";
+              format-icons = {
+                "spotify" = "󰓇";
+              };
+              persistent-workspaces = {
+                "*" = 5;
+              };
             };
-            persistent-workspaces = {
-              "*" = 5;
+
+            "hyprland/submap" = {
+              tooltip = false;
             };
-          };
 
-          "hyprland/submap" = {
-            tooltip = false;
-          };
-
-          mpris = {
-            format = "󰓇 {artist} - {title}";
-            player = "spotify";
-            on-click = "playerctl play-pause";
-            max-length = 40;
-            tooltip-format = "{artist} - {title}";
-          };
-
-          tray = {
-            icon-size = 18;
-            spacing = 10;
-          };
-
-          pulseaudio = {
-            format = "{icon} {volume}%";
-            format-bluetooth = "{icon} {volume}%";
-            format-bluetooth-muted = "󰂲";
-            format-muted = "󰖁";
-            format-icons = {
-              headphone = "󰋋";
-              hands-free = "󱡒";
-              headset = "󰋎";
-              phone = "󰄜";
-              portable = "󰦧";
-              car = "󰄋";
-              default = ["󰕿" "󰖀" "󰕾"];
+            mpris = {
+              format = "󰓇 {artist} - {title}";
+              player = "spotify";
+              on-click = "playerctl play-pause";
+              max-length = 40;
+              tooltip-format = "{artist} - {title}";
             };
-            on-click = "xdg-terminal-exec --app-id=Wiremix -e ${lib.getExe pkgs.wiremix}";
-            on-click-right = "${lib.getExe pkgs.pamixer} -t";
-          };
 
-          network = {
-            format-icons = ["󰤯" "󰤟" "󰤢" "󰤥" "󰤨"];
-            format-wifi = "{icon} {essid}";
-            format = "{icon}";
-            format-ethernet = "󰀂";
-            format-disconnected = "󰤮";
-            tooltip-format-wifi = "{essid} ({frequency} GHz)\n⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
-            tooltip-format-ethernet = "⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
-            on-click = "xdg-terminal-exec --app-id=com.niflheim.niflheim -e nmtui";
-            interval = 3;
-          };
-
-          battery = {
-            states = {
-              warning = 25;
-              critical = 10;
+            tray = {
+              icon-size = 18;
+              spacing = 10;
             };
-            format = "{icon} {capacity}%";
-            format-plugged = "󰚥";
-            format-icons = {
-              default = ["󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹"];
-              charging = ["󰢜" "󰂆" "󰂇" "󰂈" "󰢝" "󰂉" "󰢞" "󰂊" "󰂋" "󰂅"];
+
+            pulseaudio = {
+              format = "{icon} {volume}%";
+              format-bluetooth = "{icon} {volume}%";
+              format-bluetooth-muted = "󰂲";
+              format-muted = "󰖁";
+              format-icons = {
+                headphone = "󰋋";
+                hands-free = "󱡒";
+                headset = "󰋎";
+                phone = "󰄜";
+                portable = "󰦧";
+                car = "󰄋";
+                default = ["󰕿" "󰖀" "󰕾"];
+              };
+              on-click = "xdg-terminal-exec --app-id=Wiremix -e ${lib.getExe pkgs.wiremix}";
+              on-click-right = "${lib.getExe pkgs.pamixer} -t";
             };
-          };
 
-          bluetooth = {
-            format = "";
-            format-disabled = "󰂲";
-            format-connected = "";
-            tooltip-format = "Devices connected: {num_connections}";
-            on-click = lib.getExe' pkgs.blueman "blueman";
-          };
+            network = {
+              format-icons = ["󰤯" "󰤟" "󰤢" "󰤥" "󰤨"];
+              format-wifi = "{icon} {essid}";
+              format = "{icon}";
+              format-ethernet = "󰀂";
+              format-disconnected = "󰤮";
+              tooltip-format-wifi = "{essid} ({frequency} GHz)\n⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
+              tooltip-format-ethernet = "⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
+              on-click = "xdg-terminal-exec --app-id=com.niflheim.niflheim -e nmtui";
+              interval = 3;
+            };
 
-          clock = {
-            format = "{:L%A %B %d %H:%M}";
-            format-alt = " {:L%d %B W%V %Y}";
-            tooltip = false;
-          };
+            battery = {
+              states = {
+                warning = 25;
+                critical = 10;
+              };
+              format = "{icon} {capacity}%";
+              format-plugged = "󰚥";
+              format-icons = {
+                default = ["󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹"];
+                charging = ["󰢜" "󰂆" "󰂇" "󰂈" "󰢝" "󰂉" "󰢞" "󰂊" "󰂋" "󰂅"];
+              };
+            };
 
-          # cpu = {
-          #   interval = 15;
-          #   format = " ";
-          #   on-click = "xdg-terminal-exec --app-id=com.niflheim.niflheim -e ${lib.getExe pkgs.btop}";
-          # };
+            bluetooth = {
+              format = "";
+              format-disabled = "󰂲";
+              format-connected = "";
+              tooltip-format = "Devices connected: {num_connections}";
+              on-click = lib.getExe' pkgs.blueman "blueman";
+            };
+
+            clock = {
+              format = "{:L%A %B %d %H:%M}";
+              format-alt = " {:L%d %B W%V %Y}";
+              tooltip = false;
+            };
+
+            # cpu = {
+            #   interval = 15;
+            #   format = " ";
+            #   on-click = "xdg-terminal-exec --app-id=com.niflheim.niflheim -e ${lib.getExe pkgs.btop}";
+            # };
+          };
         };
+
+        style =
+          ''
+            @import "colors.css";
+
+            * {
+              font-family: "${fonts.monospace.name} Propo";
+              background-color: transparent;
+              color: @on_surface;
+              font-size: 14px;
+
+              min-height: 0;
+              margin: 0;
+              padding: 0;
+            }
+          ''
+          + (builtins.readFile ./waybar.css);
       };
-
-      style = with config.lib.stylix.colors.withHashtag;
-        ''
-          @define-color base00 ${base00}; @define-color base01 ${base01};
-          @define-color base02 ${base02}; @define-color base03 ${base03};
-          @define-color base04 ${base04}; @define-color base05 ${base05};
-          @define-color base06 ${base06}; @define-color base07 ${base07};
-          @define-color base08 ${base08}; @define-color base09 ${base09};
-          @define-color base0A ${base0A}; @define-color base0B ${base0B};
-          @define-color base0C ${base0C}; @define-color base0D ${base0D};
-          @define-color base0E ${base0E}; @define-color base0F ${base0F};
-
-          * {
-            font-family: "${config.stylix.fonts.monospace.name} Propo";
-            background-color: transparent;
-            color: @base06;
-            font-size: 14px;
-
-            min-height: 0;
-            margin: 0;
-            padding: 0;
-          }
-        ''
-        + (builtins.readFile ./waybar.css);
     };
-  };
 }
