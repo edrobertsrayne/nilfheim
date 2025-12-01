58d76b50cd1d240d0d7ed5a5132595893abe6f00
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-12-01 23:24:17 +0000

    feat(waypaper): init declarative config and trigger matugen on wallpaper change

--- modules/hyprpaper.nix
+++ modules/hyprpaper.nix
@@ -5,5 +5,23 @@ _: {
       waypaper
       hyprpaper
     ];
+
+    xdg.configFile."waypaper/config.ini".text = ''
+      [Settings]
+      language = en
+      show_path_in_tooltip = True
+      backend = hyprpaper
+      fill = fill
+      sort = name
+      color = #ffffff
+      subfolders = False
+      all_subfolders = False
+      show_hidden = False
+      show_gifs_only = False
+      zen_mode = False
+      post_command = matugen image $wallpaper
+      number_of_columns = 3
+      use_xdg_state = True
+    '';
   };
 }
