34d3ab2785b1a439a328813b313c7d5767b26598
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-12-03 20:52:39 +0000

    feat(thor): add bridge networking

---
+++ modules/hosts/thor/bridge.nix
@@ -0,0 +1,23 @@
+{inputs, ...}: {
+  flake.modules.nixos.thor = {lib, ...}: let
+    ipAddress = "192.168.68.128";
+  in {
+    networking = {
+      networkmanager.enable = lib.mkForce false;
+      useDHCP = false;
+      bridges = {
+        "br0" = {
+          interfaces = ["enp2s0" "enp3s0" "enp4s0" "enp5s0"];
+        };
+      };
+      interfaces.br0.ipv4.addresses = [
+        {
+          address = ipAddress;
+          prefixLength = 22;
+        }
+      ];
+      defaultGateway = "192.168.68.1";
+      nameservers = ["1.1.1.1" "8.8.8.8"];
+    };
+  };
+}
