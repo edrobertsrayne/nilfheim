4f04766f7e03cfb3c46c763711bd8c933e4ec08f
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-12-07 16:11:22 +0000

    feat(hyprlock): set material design lockscreen theme

--- modules/hyprlock.nix
+++ modules/hyprlock.nix
@@ -1,18 +1,73 @@
 _: {
-  flake.modules.homeManager.hyprlock = {lib, ...}: {
+  flake.modules.homeManager.hyprlock = {config, ...}: {
     programs.hyprlock = {
       enable = true;
       # TODO: get the background image from waypaper and use on lock screen
       settings = {
-        background = lib.mkForce [
+        source = ["${config.xdg.configHome}/hypr/colors.conf"];
+        background = [
           {
             monitor = "";
-            path = "screenshot";
+            path = "$image";
             blur_passes = 3;
-            blur_size = 8;
+            blur_size = 4;
+            contrast = 0.8;
+            brightness = 0.6;
+            vibrancy = 0.2;
+          }
+        ];
+
+        input-field = [
+          {
+            monitor = "";
+            size = "300, 50";
+            outline_thickness = 2;
+
+            dots_size = 0.2;
+            dots_spacing = 0.35;
+            dots_center = true;
+
+            outer_color = "$primary";
+            inner_color = "$surface_container_high";
+            font_color = "$on_surface";
+
+            fade_on_empty = false;
+            placeholder_text = "Enter password...";
+
+            position = "0, -200";
+            halign = "center";
+            valign = "center";
+          }
+        ];
+
+        label = [
+          # Time
+          {
+            monitor = "";
+            text = ''cmd[update:1000] echo "$(date +"%H:%M")"'';
+            color = "$on_surface";
+            font_size = 90;
+            font_family = "Inter";
+
+            position = "0, 200";
+            halign = "center";
+            valign = "center";
+          }
+          # Date
+          {
+            monitor = "";
+            text = ''cmd[update:60000] echo "$(date +"%A, %B %d")"'';
+            color = "$on_surface_variant";
+            font_size = 20;
+            font_family = "Inter";
+
+            position = "0, 120";
+            halign = "center";
+            valign = "center";
           }
         ];
       };
+      sourceFirst = true;
     };
   };
 }
