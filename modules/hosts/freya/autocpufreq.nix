7f777fffb5bffe9d905a6287b4a869144eb69fc3
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-12-13 22:26:51 +0000

    feat(freya): add auto-cpufreq and thermald

---
+++ modules/hosts/freya/autocpufreq.nix
@@ -0,0 +1,19 @@
+_: {
+  flake.modules.nixos.freya = {
+    services.thermald.enable = true;
+
+    services.auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    };
+  };
+}
